186d2c9f35781bb2255083cbb5d1e79f93d75eea
bamfaile <aileen.bamford@fmi.ch>
2023-05-30 13:25:00 +0200

    Folder restructuring

--- ACTB-KI/Batch20220930/Spot_intensity_analysis_utils.py
+++ ACTB-KI/Batch20220930/Spot_intensity_analysis_utils.py
@@ -55,12 +55,29 @@ def split_table_by_track(df):
     
     return df
 
+def split_table_by_ROI_and_track(df):
+    """
+    Splits each spots file into seperate dfs by ROI_ID and TRACK_ID, so that spots can be sorted and filtered by frame' 
+    """
+    df = df.groupby(["ROI_ID","TRACK_ID"], sort = False, as_index = False)
+    
+    return df
+
 def first_10_frames(df):
     """
     Sorts the split dfs by FRAME and extracts the first 10.
     """
     df['FRAME'] = df['FRAME'].astype(int) # Is probably necessary to have proper ordering of tracks by frame
     df = df.sort_values(by="FRAME").iloc[0:10]
+    
+    return df
+
+def first_5_frames(df):
+    """
+    Sorts the split dfs by FRAME and extracts the first 5.
+    """
+    df['FRAME'] = df['FRAME'].astype(int) # Is probably necessary to have proper ordering of tracks by frame
+    df = df.sort_values(by="FRAME").iloc[0:5]
     
     return df
 
@@ -76,6 +93,21 @@ def meanint_tracks_ch2(df):
     """
     df['MEAN_INTENSITY_CH2'] = df['MEAN_INTENSITY_CH2'].astype(float)
     df = df.iloc[:, np.r_[2,21]].groupby("TRACK_ID", sort = False, as_index = False).mean().round(2)
+    
+    return df
+
+def meanint_tracks_ch1(df):
+    """
+    Extracts columns "TRACK_ID" and "MEAN_INTENSITY_CH1" and calculates the mean of "MEAN_INTENSITY_CH1":
+    
+    .astypefloat converts values of 'MEAN_INTENSITY_CH1' to type 'float', so that 'groupby' operation in next line can be   perfomed
+    .iloc selects the columns TRACK_ID and MEAN_INTENSITY_CH1; the ':' in the square brackets signifies that the rows are left   untouched
+    .mean calculates mean intensities based on .groupby 'TRACK_ID'
+    .round rounds the values to 2 decimals after calculating the mean
+    
+    """
+    df['MEAN_INTENSITY_CH1'] = df['MEAN_INTENSITY_CH1'].astype(float)
+    df = df.iloc[:, np.r_[2,15]].groupby("TRACK_ID", sort = False, as_index = False).mean().round(2)
     
     return df
     
