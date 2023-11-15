a94a4328b0e93264fbc637a001f07a493c758c6c
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2023-11-15 13:45:54 +0100

    Refactor tracking script.

--- ipa/02_spot-tracking/build_config.py
+++ ipa/02_spot-tracking/build_config.py
@@ -7,25 +7,25 @@ def build_config():
 
     spots_file = questionary.path("Path spots files:").ask()
     output_dir = questionary.path("Path to output directory:").ask()
-    linkdis = float(questionary.text(
-        "linkdis:", 
+    link_distance = float(questionary.text(
+        "link_distance:",
         validate=lambda v: v.replace(".", "").isdigit()
     ).ask())
     gaps = int(questionary.text(
         "gaps:",
         validate=lambda v: v.isdigit()
     ).ask())
-    tracklen = int(questionary.text(
-        "tracklen:",
+    track_length = int(questionary.text(
+        "track_length:",
         validate=lambda v: v.isdigit()
     ).ask())
     
     config = {
         "spots_file": os.path.relpath(spots_file, cwd),
         "output_dir": os.path.relpath(output_dir, cwd),
-        "linkdis": linkdis,
+        "link_distance": link_distance,
         "gaps": gaps,
-        "tracklen": tracklen
+        "track_length": track_length
     }
 
     os.makedirs(output_dir, exist_ok=False)
--- ipa/02_spot-tracking/run_tracking.py
+++ ipa/02_spot-tracking/run_tracking.py
@@ -1,82 +1,88 @@
-from multiprocessing import Pool
-import os
-from glob import glob
-import pandas as pd
-import numpy as np
-import yaml
-
-import trackpy as tp
-
-from numpy.typing import ArrayLike
-
-from tqdm import tqdm
 import logging
+import os
 from datetime import datetime
+from glob import glob
+from os.path import join
 
-from os.path import join, basename
+import numpy as np
+import pandas as pd
+import trackpy as tp
+import yaml
+from numpy._typing import ArrayLike
+from tqdm import tqdm
 
 logger = logging.Logger('Tracking')
 now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-handler = logging.FileHandler(f"{now}-spot-detection.log") # How to replace?
+handler = logging.FileHandler(f"{now}-spot-tracking.log")
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-def load_data(spots_file: str):
-    """Load file containig detected spots.
-    
+
+def split_table_by_roi_id(spots: pd.DataFrame) -> list[pd.DataFrame]:
+    """
+    Splits each spots file into seperate dfs by roi-id, so that tracking can
+    be done per ROI.
+
     Parameter:
-        spots_file: Path to spots file.
+        spots: DataFrame containing spots
 
     Returns:
-        spots: Loaded spots file
+        spots_per_ROI: List of DataFrames containing spots per ROI
     """
-    spots = pd.read_csv(spots_file)
+    return [j for i,j in spots.groupby("roi_id", sort = False, as_index = False)]
 
-    return spots
 
-def split_table_by_roi_id(spots: ArrayLike):
-    """
-    Splits each spots file into seperate dfs by roi-id, so that tracking can be done per ROI 
-    """
-    spots_per_ROI = [j for i,j in spots.groupby("roi_id", sort = False, as_index = False)]
-    
-    return spots_per_ROI
-
-def linking(spots_per_ROI: list, linkdis: float, gaps: int, tracklen: int):
+def linking(
+    spots_file: str,
+    link_distance: float,
+    gaps: int,
+    track_length: int
+):
     """
     Links spots in dataframes with trackpy's basic linking function.
 
     Parameter:
-        spots_per_ROI: list of spots per ROI
+        spots_file: path to spots file
+        link_distance: maximum distance between two spots to be linked
+        gaps: maximum number of frames a spot can be missing
+        track_length: minimum length of track to be included in the output
 
     Returns:
         tracks: DataFrame containing column 'Particle', representing linked particles
     """
+    spots = pd.read_csv(spots_file)
+    spots_per_roi = split_table_by_roi_id(spots)
    
-    tracks_per_ROI = []
+    tracks_per_roi = []
 
-    for df in spots_per_ROI:
-        df = tp.link(df, linkdis, memory = gaps)
-        df = tp.filter_stubs(df, tracklen)
-        tracks_per_ROI.append(df)
+    for df in spots_per_roi:
+        df = tp.link(df, link_distance, memory=gaps)
+        df = tp.filter_stubs(df, track_length)
+        tracks_per_roi.append(df)
     
-    tracks = pd.concat(tracks_per_ROI)
+    return pd.concat(tracks_per_roi)
 
-    return tracks
 
-def create_track_id(row):
+def create_track_id(row: pd.Series) -> str:
     """
     Funtion to create unique track-ids for all tracks of all ROIs per image by combining roi_id
     and particle number.
     """
     return f"{row['roi_id']}_{row['particle']}"
 
-def apply_track_id(tracks: ArrayLike):
+
+def add_track_id_column(tracks: pd.DataFrame) -> pd.DataFrame:
     """
     Creates track_id by ROI and particle number.
     Sorts values by track_id and frame.
+
+    Parameter:
+        tracks: DataFrame containing tracks
+
+    Returns:
+        tracks: DataFrame containing tracks with track_id column
     """
     tracks['track_id'] = tracks.apply(create_track_id, axis=1)
     tracks.index._name = 'index'
@@ -84,9 +90,16 @@ def apply_track_id(tracks: ArrayLike):
 
     return tracks
 
-def uniqueid(tracks: ArrayLike):
+
+def add_unique_id_column(tracks: pd.DataFrame) -> pd.DataFrame:
     """
-    Creates an additional column that contains unique track ids for easy visualization in Napari.    
+    Creates an additional column that contains unique track ids for easy visualization in Napari.
+
+    Parameter:
+        tracks: DataFrame containing tracks
+
+    Returns:
+        tracks: DataFrame containing tracks with unique_id column
     """
     tracks['unique_id'] = tracks['track_id']
     track_ids = tracks['unique_id'].unique()
@@ -107,13 +120,17 @@ if __name__ == "__main__":
         logger.info(f"Processing file: {spots_file}")
 
         tracks_per_img = linking(
-            spots_file = spots_file,
-            linkdis=config['linkdis'],
+            spots_file=spots_file,
+            link_distance=config['linkdis'],
             gaps=config['gaps'],
-            tracklen=config['tracklen']
+            track_length=config['tracklen']
         )
 
+        tracks_per_img = add_track_id_column(tracks_per_img)
+
+        tracks_per_img = add_unique_id_column(tracks_per_img)
+
         name, _ = os.path.splitext(os.path.basename(spots_file))
-        tracks_per_img.to_csv(os.path.join(config['output_dir'], f"{name}_tracks.csv"), index=False)
+        tracks_per_img.to_csv(join(config['output_dir'], f"{name}_tracks.csv"), index=False)
 
     logger.info("Done!")
