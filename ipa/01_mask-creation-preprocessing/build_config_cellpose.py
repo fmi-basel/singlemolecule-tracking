64782ef914738f71e3bc2025ba831927bc1bb1c6
bamfaile <aileen.bamford@fmi.ch>
2025-04-25 13:43:46 +0200

    Clean-up notebook corrections and addition of cellpose script

---
+++ ipa/01_mask-creation-preprocessing/build_config_cellpose.py
@@ -0,0 +1,49 @@
+import os
+import questionary
+import yaml
+
+
+def build_config():
+    cwd = os.getcwd()
+
+    input_path = questionary.path("Input path max projections:").ask()
+    whole_cell_model = questionary.path("Path to model:").ask()
+
+    n_iter = int(
+        questionary.text(
+            "Number of iterations:",
+            default="2000",
+            validate=lambda v: v.isdigit()
+        ).ask()
+    )
+
+    cellprob_threshold = float(
+        questionary.text(
+            "Cell probability threshold (float):",
+            default="0.0",
+            validate=lambda v: v.replace(".", "", 1).isdigit() and v.count(".") <= 1
+        ).ask()
+    )
+
+    flow_threshold = float(
+        questionary.text(
+            "Flow threshold (float):",
+            default="0.9",
+            validate=lambda v: v.replace(".", "", 1).isdigit() and v.count(".") <= 1
+        ).ask()
+    )
+
+    config = {
+        "input_path": os.path.relpath(input_path, cwd),
+        "whole_cell_model": os.path.relpath(whole_cell_model, cwd),
+        "n_iter": n_iter,
+        "cellprob_threshold": cellprob_threshold,
+        "flow_threshold": flow_threshold,
+    }
+
+    with open(os.path.join(cwd, "cellpose_config.yaml"), "w") as f:
+        yaml.safe_dump(config, f)
+
+
+if __name__ == "__main__":
+    build_config()
